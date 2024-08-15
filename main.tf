71f6ffb126a8bad15f0e3b3008f40cf6d8103379
Laura Bermudez <laurab1528@hotmail.com>
2024-08-15 14:32:59 -0500

    update files

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Obtener las zonas de disponibilidad
+data "aws_availability_zones" "available" {}
+
 # Crear el VPC
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
@@ -13,12 +16,15 @@ resource "aws_internet_gateway" "main" {
 }
 
 # Crear la subred pública
-resource "aws_subnet" "main" {
-  count                  = 2
-  vpc_id                 = aws_vpc.main.id
-  cidr_block             = element(var.vpc_subnet_cidr, count.index)
-  availability_zone      = element(var.aws_availability_zones, count.index)
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.vpc_subnet_cidr, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
 }
 
 # Crear la tabla de rutas para la subred pública
@@ -33,11 +39,10 @@ resource "aws_route_table" "public" {
 
 # Asociar la tabla de rutas con la subred pública
 resource "aws_route_table_association" "public" {
-  count            = 2
-  subnet_id        = aws_subnet.main[count.index].id
-  route_table_id   = aws_route_table.public.id
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
-
 
 # Crear el grupo de seguridad para el ALB
 resource "aws_security_group" "elb_sg" {
@@ -60,9 +65,7 @@ resource "aws_security_group" "elb_sg" {
 
 # Crear el grupo de seguridad para las instancias EC2
 resource "aws_security_group" "ec2_sg" {
-  name        = "my-security-group"
-  description = "Security group for EC2 instances"
-  vpc_id      = "vpc-12345678"  # Asegúrate de que este ID sea correcto
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 80
@@ -79,34 +82,27 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-
 # Crear las instancias EC2
 resource "aws_instance" "web" {
   count         = 2
   ami           = var.instance_ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
   security_groups = [aws_security_group.ec2_sg.name]
-
-
 }
-
 
 # Crear el Application Load Balancer
 resource "aws_lb" "web_lb" {
   name               = "unique-web-lb-name"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ec2_sg.id]
+  security_groups    = [aws_security_group.elb_sg.id]
   subnets            = aws_subnet.public.*.id
   enable_deletion_protection = false
 
   enable_cross_zone_load_balancing = true
   enable_http2                       = true
 }
-
-
-
 
 # Crear el listener para el ALB
 resource "aws_lb_listener" "http" {
